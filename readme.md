8b20d28ef1e01ed6a052400be2a59f871a037f89
Jonathan Stines <jonathan.stines@gmail.com>
2023-03-27 08:20:51 -0500

    initial commit

---
+++ readme.md
@@ -0,0 +1,35 @@
+# 💰 Salary Checker 💰
+This is a Python program that checks the median salaries of software engineers for companies that have a job posting with a certain keyword in their job description. It uses the go-dork command line tool to search for job postings, and the levels.fyi website to retrieve salary information for a given company.
+
+## Usage
+```bash
+python salarychecker.py [job_characteristic] [-h | --help]
+```
+
+## Options
+* `-j job_characteristic`, `--job job_characteristic` - Job characteristic to search for on job listing websites
+* `-c company`, `--company company` - Name of a specific company to search for salary information
+* `-h`, `--help` - Optional. Displays the help menu.
+
+## Example
+```bash
+python salarychecker.py -j "Penetration Test"
+python salarychecker.py -j "OSCP"
+
+python salarychecker.py -c "Salesforce"
+python salarychecker.py -c "Rapid7"
+```
+
+## Installation
+1. Clone the repository: git clone https://github.com/fr4nk3nst1ner/salarychecker.git
+2. Install the required Python packages
+3. Install [go-dork](https://github.com/dwisiswant0/go-dork)
+4. Run the program: python salarychecker.py [job_title]
+
+## Requirements
+* Python 3.6 or higher
+* go-dork command line tool
+* BeautifulSoup and requests Python packages
+
+## Disclaimer
+This program is for educational and informational purposes only. The salary information provided is not guaranteed to be accurate or up-to-date.
---
+++ salarysleuth.py
@@ -0,0 +1,124 @@
+import argparse
+import re
+import subprocess
+import requests
+from bs4 import BeautifulSoup
+
+
+BANNER = """
+\033[38;5;196m$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+$$$                                                                          $$$
+$$$                               SALARY CHECKER                             $$$
+$$$                                                                          $$$
+$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+\033[0m
+"""
+
+
+def get_job_urls(query):
+    # Define the go-dork command
+    godork_cmd = f'go-dork -e google -p 50 -s -q "{query}"'
+
+    # Run the go-dork command and capture the output
+    output = subprocess.check_output(godork_cmd, shell=True)
+
+    # Decode the output as a string
+    output_str = output.decode('utf-8')
+
+    # Extract the URLs from the output using regular expressions
+    job_urls = re.findall(r'https?://\S+', output_str)
+
+    return job_urls
+
+
+def get_salary(company_name):
+    url = f"https://www.levels.fyi/companies/{company_name}/salaries/"
+    response = requests.get(url)
+    html = response.content.decode()
+
+    # Extract salary info using BeautifulSoup
+    soup = BeautifulSoup(html, "html.parser")
+    salary_elem = soup.find("td", text="Software Engineer Salary")
+    if salary_elem:
+        salary = salary_elem.find_next_sibling("td").text
+        return salary
+
+    return "No Entries on levels.fyi :("
+
+
+def get_company_name(url):
+    # Extract the company name from the URL
+    match = re.search(r'\/\/[^/]+\/([^/]+)\/', url)
+    if match:
+        company_name = match.group(1)
+    else:
+        company_name = ''
+
+    return company_name
+
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Salary Checker Tool")
+    parser.add_argument("-j", "--job", type=str, help="Job characteristic to search for on job listing websites")
+    parser.add_argument("-c", "--company", type=str, help="Name of a specific company to search for salary information")
+
+    args = parser.parse_args()
+
+    if not args.job and not args.company:
+        print("Please provide a job title or company name to search for. Use --help for usage details.")
+        return
+
+    if args.job:
+        dork_query = f"site:lever.co OR site:greenhouse.io {args.job}"
+        job_urls = get_job_urls(dork_query)
+
+        # uncomment these lines if you want unique companeies only 
+        #seen_companies = set()
+        for url in job_urls:
+            company_name = get_company_name(url)
+        #    if company_name in seen_companies:
+        #        continue
+        #    else:
+        #        seen_companies.add(company_name)
+
+            median_salary = get_salary(company_name)
+
+            print(f"Job URL: {url}")
+            print(f"Company: {company_name}")
+            if median_salary == "No Entries on levels.fyi :(":
+                print(f"Median Salary for Software Engineer: {median_salary}")
+            else:
+                median_salary = int(median_salary.replace(",", "").replace("$", ""))
+                if median_salary >= 300000:
+                    print(f"Median Salary for Software Engineer: \033[32m{median_salary:,}\033[0m")
+                elif median_salary >= 200000:
+                    print(f"Median Salary for Software Engineer: \033[92m{median_salary:,}\033[0m")
+                elif median_salary >= 100000:
+                    print(f"Median Salary for Software Engineer: \033[93m{median_salary:,}\033[0m")
+                else:
+                    print(f"Median Salary for Software Engineer: \033[31m{median_salary:,}\033[0m")
+            print("-" * 50)
+
+    if args.company:
+        median_salary = get_salary(args.company)
+        print(f"Company: {args.company}")
+        if median_salary == "No Entries on levels.fyi :(":
+            print(f"Median Salary for Software Engineer: {median_salary}")
+        else:
+            median_salary = int(median_salary.replace(",", "").replace("$", ""))
+            if median_salary >= 300000:
+                print(f"Median Salary for Software Engineer: \033[32m{median_salary:,}\033[0m")
+            elif median_salary >= 200000:
+                print(f"Median Salary for Software Engineer: \033[92m{median_salary:,}\033[0m")
+            elif median_salary >= 100000:
+                print(f"Median Salary for Software Engineer: \033[93m{median_salary:,}\033[0m")
+            else:
+                print(f"Median Salary for Software Engineer: \033[31m{median_salary:,}\033[0m")
+        print("-" * 50)
+
+
+if __name__ == "__main__":
+    print(BANNER)
+    main()
+
