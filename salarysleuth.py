a692ac25d8cdce5db57c713877324f2bc4052e51
Jonathan Stines <jonathan.stines@gmail.com>
2023-04-18 16:57:04 -0500

    Update salarysleuth.py
    added table flag to change output

--- salarysleuth.py
+++ salarysleuth.py
@@ -43,9 +43,12 @@ def get_salary(company_name):
     salary_elem = soup.find("td", string="Software Engineer Salary")
     if salary_elem:
         salary = salary_elem.find_next_sibling("td").text
-        return salary
+        salary = int(salary.replace(",", "").replace("$", ""))
+        salary = float(salary) if salary else None
+        return {'company': company_name, 'salary': salary}
 
-    return "No Entries on levels.fyi :("
+    return None
+    #return "No Entries on levels.fyi :("
 
 
 def get_company_name(url):
@@ -62,6 +65,20 @@ def print_banner(silence):
     if not silence:
         print(BANNER)
 
+def colorize_salary(salary):
+    if salary is None:
+        return "No Data"
+    elif salary >= 300000:
+        return f"\033[32m${salary:,.0f}\033[0m"
+    elif salary >= 200000:
+        return f"\033[92m${salary:,.0f}\033[0m"
+    elif salary >= 100000:
+        return f"\033[93m${salary:,.0f}\033[0m"
+    else:
+        return f"\033[31m${salary:,.0f}\033[0m"
+
+
+
 def main():
     parser = argparse.ArgumentParser(description="Examples: \n python salarysleuth.py -j kali \n python salarysleuth.py -j oscp \n python salarysleuth.py -c rapid7 \n python salarysleuth.py -c salesforce", formatter_class=RawTextHelpFormatter,usage=SUPPRESS)
     parser.add_argument("-j", "--job", type=str, help="Job characteristic to search for on job listing websites")
@@ -69,6 +86,7 @@ def main():
     parser.add_argument("-s", "--silence", action="store_true", help="Silence the banner")
     parser.add_argument("-p", "--pages", type=int, default=50, help="Number of search result pages to scrape (default: 50)")
     parser.add_argument("-e", "--engine", type=str, default='google', help="Search engine to use (default: google). \n Options: Google, Shodan, Bing, Duck, Yahoo, Ask \n Note: Only tested with Google")
+    parser.add_argument("-t", "--table", action="store_true", help="Re-organize output into a table in ascending order based on median salary")
 
     args = parser.parse_args()
 
@@ -83,48 +101,59 @@ def main():
         dork_query = f"site:lever.co OR site:greenhouse.io {args.job}"
         job_urls = get_job_urls(dork_query, args.pages, args.engine)
 
-        # uncomment these lines if you want unique companeies only 
-        #seen_companies = set()
+        salaries = []
         for url in job_urls:
             company_name = get_company_name(url)
-        #    if company_name in seen_companies:
-        #        continue
-        #    else:
-        #        seen_companies.add(company_name)
+            salary_dict = get_salary(company_name)
+            if salary_dict is not None:
+                salary_dict['url'] = url  # Add URL to salary_dict
+                salaries.append(salary_dict)
 
-            median_salary = get_salary(company_name)
+        if args.table:
+            # Sort the salaries list based on the median salary
+            salaries = sorted(salaries, key=lambda x: x['salary'], reverse=True)
 
-            print(f"Job URL: {url}")
-            print(f"Company: \033[35m{company_name}\033[0m")
-            if median_salary == "No Entries on levels.fyi :(":
-                print(f"Median Total Comp for Software Engineer: {median_salary}")
-            else:
-                median_salary = int(median_salary.replace(",", "").replace("$", ""))
-                if median_salary >= 300000:
-                    print(f"Median Total Comp for Software Engineer: \033[32m{median_salary:,}\033[0m")
-                elif median_salary >= 200000:
-                    print(f"Median Total Comp for Software Engineer: \033[92m{median_salary:,}\033[0m")
-                elif median_salary >= 100000:
-                    print(f"Median Total Comp for Software Engineer: \033[93m{median_salary:,}\033[0m")
+            # Print the table header
+            print("\033[1m{:<16} {:<16} {:<50}\033[0m".format("Company Name", "Median Salary", "Job URL"))
+
+
+            # Print each row in the table
+            for salary in salaries:
+                print("{:<25} {:<25} {:<50}".format("\033[35m" + salary['company'] + "\033[0m", colorize_salary(salary['salary']), salary['url']))
+
+        else:
+            for salary in salaries:
+                print(f"Job URL: {url}")
+                print(f"Company: \033[35m{salary['company']}\033[0m")
+                if salary['salary'] is None:
+                    print("No salary information found for this company.")
                 else:
-                    print(f"Median Total Comp for Software Engineer: \033[31m{median_salary:,}\033[0m")
-            print("-" * 50)
+                    median_salary = salary['salary']
+                    if median_salary >= 300000:
+                        print(f"Median Total Comp for Software Engineer: \033[32m${median_salary:,}\033[0m")
+                    elif median_salary >= 200000:
+                        print(f"Median Total Comp for Software Engineer: \033[92m${median_salary:,}\033[0m")
+                    elif median_salary >= 100000:
+                        print(f"Median Total Comp for Software Engineer: \033[93m${median_salary:,}\033[0m")
+                    else:
+                        print(f"Median Total Comp for Software Engineer: \033[31m${median_salary:,}\033[0m")
+                print("-" * 50)
 
     if args.company:
         median_salary = get_salary(args.company)
-        print(f"Company: \033[35m{args.company}\033[0m")
-        if median_salary == "No Entries on levels.fyi :(":
-            print(f"Median Salary for Software Engineer: {median_salary}")
+        print(f"Company: \033[35m{median_salary['company']}\033[0m")
+        if median_salary['salary'] is None:
+            print("No salary information found for this company.")
         else:
-            median_salary = int(median_salary.replace(",", "").replace("$", ""))
+            median_salary = median_salary['salary']
             if median_salary >= 300000:
-                print(f"Median Total Comp for Software Engineer: \033[32m{median_salary:,}\033[0m")
+                print(f"Median Total Comp for Software Engineer: \033[32m${median_salary:,}\033[0m")
             elif median_salary >= 200000:
-                print(f"Median Total Comp for Software Engineer: \033[92m{median_salary:,}\033[0m")
+                print(f"Median Total Comp for Software Engineer: \033[92m${median_salary:,}\033[0m")
             elif median_salary >= 100000:
-                print(f"Median Total Comp for Software Engineer: \033[93m{median_salary:,}\033[0m")
+                print(f"Median Total Comp for Software Engineer: \033[93m${median_salary:,}\033[0m")
             else:
-                print(f"Median Total Comp for Software Engineer: \033[31m{median_salary:,}\033[0m")
+                print(f"Median Total Comp for Software Engineer: \033[31m${median_salary:,}\033[0m")
         print("-" * 50)
 
 
