b8c189ed82c09995741d6295ffc35654bac4aa63
Jonathan Stines <jonathan.stines@gmail.com>
2023-05-08 10:04:36 -0500

    optimized and refactored, was previously taking too long

--- salarysleuth.py
+++ salarysleuth.py
@@ -18,7 +18,7 @@ $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 """
 
 
-def get_job_urls(query, num_pages, search_engine, remote_only=False):
+def get_job_urls(query, num_pages, search_engine, remote_only=False, include_job_title=False):
     # Define the go-dork command
     godork_cmd = f'go-dork -e {search_engine} -p {num_pages} -s -q "{query}"'
 
@@ -28,40 +28,48 @@ def get_job_urls(query, num_pages, search_engine, remote_only=False):
     # Decode the output as a string
     output_str = output.decode('utf-8')
 
-    # Extract the URLs from the output using regular expressions
-    job_urls = re.findall(r'https?://\S+', output_str)
+    # Extract the URLs and job titles from the output using regular expressions
+    job_urls = []
+    job_titles = []
+    for match in re.findall(r'(https?://\S+)[\n\s]+([^\n]+)', output_str):
+        url, title = match
+        job_urls.append(url)
+        job_titles.append(title.strip())
 
-    # Extract the job titles from the URLs using BeautifulSoup
-    job_data = []
-    for url in job_urls:
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, 'html.parser')
-        if 'lever.co' in url:
-            commitment_elem = soup.find('div', {'class': 'sort-by-commitment posting-category medium-category-label commitment'})
-            location_elem = soup.find('div', {'class': 'location'})
-            if remote_only and (not commitment_elem or 'remote' not in commitment_elem.text.lower()):
-                continue
-            title_elem = soup.find('h2')
-            if title_elem:
-                title = title_elem.text.strip()
+    # Filter job URLs by remote only if the remote_only flag is set
+    if remote_only:
+        remote_job_urls = []
+        remote_job_titles = []
+        for url, title in zip(job_urls, job_titles):
+            if 'lever.co' in url:
+                response = requests.get(url)
+                html = response.content.decode()
+                soup = BeautifulSoup(html, 'html.parser')
+                commitment_elem = soup.find('div', {'class': 'sort-by-commitment posting-category medium-category-label commitment'})
+                location_elem = soup.find('div', {'class': 'location'})
+                title_elem = soup.find('h2')
+                if commitment_elem and commitment_elem.text.lower() == 'remote' and (not location_elem or 'remote' in location_elem.text.lower()):
+                    remote_job_urls.append(url)
+                    remote_job_titles.append(title_elem.text.strip() if title_elem else '')
+            elif 'greenhouse.io' in url:
+                response = requests.get(url)
+                html = response.content.decode()
+                soup = BeautifulSoup(html, 'html.parser')
+                location_elem = soup.find('div', {'class': 'location'})
+                title_elem = soup.find('h1')
+                if location_elem and 'remote' in location_elem.text.lower():
+                    remote_job_urls.append(url)
+                    remote_job_titles.append(title_elem.text.strip() if title_elem else '')
             else:
-                title = ''
-        elif 'greenhouse.io' in url:
-            location_elem = soup.find('div', {'class': 'location'})
-            if remote_only and (not location_elem or 'remote' not in location_elem.text.lower()):
-                continue
-            title_elem = soup.find('h1')
-            if title_elem:
-                title = title_elem.text.strip()
-            else:
-                title = ''
-        else:
-            title = ''
-        job_data.append({'title': title, 'url': url})
+                remote_job_urls.append(url)
+                remote_job_titles.append(title)
+        job_urls = remote_job_urls
+        job_titles = remote_job_titles
 
-    return job_data
-
-
+    if include_job_title:
+        return [(job_url, job_title) for job_url, job_title in zip(job_urls, job_titles)]
+    else:
+        return job_urls
 
 
 
@@ -83,9 +91,9 @@ def get_salary(company_name, use_decimal=False):
 
     return None
 
-def get_company_name(url_dict):
-    # Extract the company name from the URL dictionary
-    url = url_dict['url']
+def get_company_name(url):
+
+    # Extract the company name from the URL
     match = re.search(r'\/\/[^/]+\/([^/]+)\/', url)
     if match:
         company_name = match.group(1)
@@ -93,7 +101,6 @@ def get_company_name(url_dict):
         company_name = ''
 
     return company_name
-
 
 def print_banner(silence):
     if not silence:
@@ -124,7 +131,7 @@ def main():
     parser.add_argument("-p", "--pages", type=int, default=50, help="Number of search result pages to scrape (default: 50)")
     parser.add_argument("-e", "--engine", type=str, default='google', help="Search engine to use (default: google). \n Options: Google, Shodan, Bing, Duck, Yahoo, Ask \n Note: Only tested with Google")
     parser.add_argument("-t", "--table", action="store_true", help="Re-organize output into a table in ascending order based on median salary")
-    parser.add_argument("-r", "--remote", action="store_true", help="Retrieve only remote jobs")
+    parser.add_argument("-r", "--remote", action="store_true", help="Search for remote jobs only")
 
     args = parser.parse_args()
 
@@ -136,58 +143,72 @@ def main():
 
     # do stuff if `-j` is passed, regardless if `-t` is passed 
     if args.job:
-        dork_query = f"site:lever.co OR site:greenhouse.io {args.job}"
-        job_urls = get_job_urls(dork_query, args.pages, args.engine, remote_only=args.remote)
+            dork_query = f"site:lever.co OR site:greenhouse.io {args.job}"
+            job_urls = get_job_urls(dork_query, args.pages, args.engine, args.remote)
+        
 
-
-        # prep salaries / url dict
-        salaries = []
-        for url_dict in tqdm(job_urls, desc='Processing job URLs'):
-            url = url_dict['url']
-            job_title = url_dict['title']
-            company_name = get_company_name(url_dict)
-            salary_dict = get_salary(company_name)
-            if salary_dict is not None:
-                # Add URL, job title, and colorized salary to salary_dict
-                salary_dict['url'] = url
-                salary_dict['title'] = job_title
-                salary_dict['salary'] = colorize_salary(salary_dict['salary']) if salary_dict['salary'] else 'No Data'
-                salaries.append(salary_dict)
-            else:
-                # Add company name, job title, and None salary to salaries list
-                salaries.append({'company': company_name, 'title': job_title, 'salary': None, 'url': url})
-                        
-
-        # do stuff if `-t` is passed 
-        if args.table:
-            # Remove entries with no salary data
-            salaries = [s for s in salaries if s['salary'] is not None]
-
-            # Sort the salaries list based on the median salary
-            salaries = sorted(salaries, key=lambda x: int(re.sub(r'\x1b\[\d+m|\$', '', x['salary']).replace(',', '')) if isinstance(x['salary'], str) else x['salary'], reverse=True)
-
-            # Print the table header
-            print("\033[1m{:<16} {:<16} {:<50} {:<50}\033[0m".format("Company Name", "Median Salary", "Job Title", "Job URL"))
-
-            # Print each row in the table
-            for salary in salaries:
-                print("{:<25} {:<25} {:<50} {:<50}".format("\033[35m" + salary['company'] + "\033[0m", salary['salary'], salary['title'], salary['url']))
-
-
-        # do stuff if `-t` isn't passed 
-        else:
-            for url_dict, salary in zip(job_urls, salaries):
-                url = url_dict['url']
-                job_title = url_dict['title']
-                print(f"Job URL: {url}")
-                print(f"Job Title: {job_title}")
-                print(f"Company: \033[35m{salary['company']}\033[0m")
-                if salary['salary'] is None:
-                    print("No salary information found for this company.")
+            salaries = []
+            for url in tqdm(job_urls, desc='Processing job URLs'):
+                company_name = get_company_name(url)
+                salary_dict = get_salary(company_name)
+                if salary_dict is not None:
+                    # Add URL to salary_dict
+                    salary_dict['url'] = url  
+                    salary_dict['salary'] = colorize_salary(salary_dict['salary']) if salary_dict['salary'] else 'No Data'
+                    title = ''
+                    if 'lever.co' in url:
+                        response = requests.get(url)
+                        html = response.content.decode()
+                        soup = BeautifulSoup(html, 'html.parser')
+                        title_elem = soup.find('h2')
+                        if title_elem:
+                            title = title_elem.text.strip()
+                        else:
+                            title = ''
+                    elif 'greenhouse.io' in url:
+                        response = requests.get(url)
+                        html = response.content.decode()
+                        soup = BeautifulSoup(html, 'html.parser')
+                        title_elem = soup.find('h1')
+                        if title_elem:
+                            title = title_elem.text.strip()
+                        else:
+                            title = ''
+                    salary_dict['title'] = title
+                    salaries.append(salary_dict)
                 else:
-                    median_salary = salary['salary']
-                    print(f"Median Total Comp for Software Engineer: {colorize_salary(median_salary)}")
-                print("-" * 50)
+                    # Add company name and None salary to salaries list
+                    salaries.append({'company': company_name, 'salary': None, 'url': url, 'title': ''})  
+                    
+            # do stuff if `-t` is passed 
+            if args.table:
+                # Remove entries with no salary data
+                salaries = [s for s in salaries if s['salary'] is not None]
+
+                # Sort the salaries list based on the median salary
+                salaries = sorted(salaries, key=lambda x: int(re.sub(r'\x1b\[\d+m|\$', '', x['salary']).replace(',', '')) if isinstance(x['salary'], str) else x['salary'], reverse=True)
+
+                # Print the table header
+                print("\033[1m{:<16} {:<16} {:<50} {:<50}\033[0m".format("Company Name", "Median Salary", "Job Title", "Job URL"))
+
+                # Print each row in the table
+                for salary in salaries:
+                    print("{:<25} {:<25} {:<50} {:<50}".format("\033[35m" + salary['company'] + "\033[0m", salary['salary'], salary['title'], salary['url']))
+                    
+            # do stuff if `-t` isn't passed 
+            else:
+                for salary, url in zip(salaries, job_urls):
+                    print(f"Job URL: {url}")
+                    print(f"Company: \033[35m{salary['company']}\033[0m")
+                    if 'title' in salary:
+                        print(f"Job Title: \033[35m{salary['title']}\033[0m")
+                    if salary['salary'] is None:
+                        print("No salary information found for this company.")
+                    else:
+                        median_salary = salary['salary']
+                        print(f"Median Total Comp for Software Engineer: {colorize_salary(median_salary)}")
+                    print("-" * 50)
+
 
 
     # do stuff if `-c` is passed 
